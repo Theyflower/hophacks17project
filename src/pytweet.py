875591191594d706dcfe0d93c9441e8aaa36eaeb
Aman Agrawal <aa3539@drexel.edu>
2017-09-16 10:07:14 -0400

    Added Auth

---
+++ src/pytweet.py
@@ -0,0 +1,12 @@
+import tweepy
+
+consumer_key = 'your consumer_key'
+consumer_secret = 'your consumer_secret'
+access_token = 'your access_token'
+access_token_secret = 'your access_token_secret'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+

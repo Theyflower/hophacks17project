827b5acfb47ad33ced33fae03c91e105bb957c6f
Aaron Thomas <1d20m6@gmail.com>
2017-09-16 11:42:53 -0400

    Updated how oauth keys are handled.
    Please run `$ git update-index --assume-unchanged *config.py` in src/ to
    make sure you don't commit keys onto github.

--- src/pytweet.py
+++ src/pytweet.py
@@ -1,9 +1,11 @@
+import twitter_config
+
 import tweepy
 
-consumer_key = 'your consumer_key'
-consumer_secret = 'your consumer_secret'
-access_token = 'your access_token'
-access_token_secret = 'your access_token_secret'
+consumer_key = twitter_config.CONSUMER_KEY
+consumer_secret = twitter_config.CONSUMER_SECRET
+access_token = twitter_config.ACCESS_TOKEN
+access_token_secret = twitter_config.ACCESS_TOKEN_SECRET
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)

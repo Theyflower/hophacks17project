68f35cb04fecffc47f585c9d9d5f7e3cba30a9a8
Aaron Thomas <1d20m6@gmail.com>
2017-09-16 14:28:38 -0400

    Please learn to read documentation

--- src/giphy.py
+++ src/giphy.py
@@ -14,7 +14,7 @@ random_sad_images()
 
 FFmpeg(
 	inputs={'https://i.giphy.com/media/10tIjpzIu8fe0/giphy.gif': None},
-	outputs={'output.gif': ["-vf \"drawtext=fontfile=/usr/share/fonts/default/Type1/n019003l.pfb:text='hey': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5:boxborderw=5: x=(w-text_w)/2: y=(h-text_h)-2\" -an"}
+	outputs={'output.gif': ["-vf", "drawtext=fontfile=/usr/share/fonts/default/Type1/n019003l.pfb:text='hey': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5:boxborderw=5: x=(w-text_w)/2: y=(h-text_h)-2", "-an"]}
 )
 
 if __name__ == '__main__':
